9420f32b82ecbe89a6ce116ade7d827d7ecb5a87
jeongchaeuk <jeongchaeuk@gmail.com>
2021-03-14 00:57:10 +0900

    Solve sudoku
    check row, column and group blocks repeatly

--- app/src/main/java/com/jeongchaeuk/sudoku/MainActivity.kt
+++ app/src/main/java/com/jeongchaeuk/sudoku/MainActivity.kt
@@ -29,27 +29,92 @@ const val GROUP_SIZE = 3
 
 class Sudoku(var board: List<Block> = List(SIZE * SIZE) { Block(it) }) {
 
+    fun makeSample() {
+        setValue(2, 1)
+        setValue(5, 6)
+        setValue(7, 4)
+        setValue(9, 5)
+        setValue(14, 9)
+        setValue(15, 3)
+        setValue(19, 6)
+        setValue(20, 2)
+        setValue(25, 1)
+        setValue(26, 9)
+        setValue(31, 7)
+        setValue(32, 8)
+        setValue(36, 8)
+        setValue(39, 9)
+        setValue(41, 1)
+        setValue(44, 5)
+        setValue(48, 6)
+        setValue(49, 3)
+        setValue(54, 6)
+        setValue(55, 4)
+        setValue(60, 2)
+        setValue(61, 5)
+        setValue(65, 9)
+        setValue(66, 5)
+        setValue(71, 6)
+        setValue(73, 5)
+        setValue(75, 8)
+        setValue(78, 9)
+    }
+
+    override fun toString(): String {
+        var result = ""
+        var count = 0
+        var groupCol = 0
+        var groupRow = 0
+        for (block in board) {
+            result += block.toString()
+            if (++groupCol == 3) {
+                result += " | "
+                groupCol = 0
+            }
+            if (++groupRow == 27) {
+                result += List(9 * 11 + 2 * 3, { "-" }).joinToString("")
+                groupRow = 0
+            }
+
+            if (++count == SIZE) {
+                result += "\n"
+                count = 0
+            }
+        }
+
+        return result
+    }
+
     fun getBlock(row: Int, col: Int): Block {
         return board.get(index = row * SIZE + col)
     }
 
-    private fun getSameRow(target: Block): List<Block> {
+    /**
+     * Get same row blocks
+     */
+    private fun getSameRowOf(target: Block): List<Block> {
         val row = target.index / SIZE
         val start = row * SIZE
         return board.subList(start, start + SIZE)
     }
 
-    private fun getSameCol(target: Block): List<Block> {
-        val col = (target.index % SIZE)
+    /**
+     * Get same column blocks
+     */
+    private fun getSameColumnOf(block: Block): List<Block> {
+/*
+        val col = (block.index % SIZE)
         val list = mutableListOf<Block>()
-
-        for (i in 0 until SIZE) {
+        for (i in 0 until SIZE)
             list.add(board[col + i * SIZE])
-        }
-
         return list
+*/
+        return List<Block>(SIZE) { board[block.index % SIZE + it * SIZE] }
     }
 
+    /**
+     * Get 3 x 3 blocks
+     */
     private fun getSameGroup(target: Block): List<Block> {
 /*
         val row = target.index / SIZE
@@ -76,29 +141,148 @@ class Sudoku(var board: List<Block> = List(SIZE * SIZE) { Block(it) }) {
         return list
     }
 
-    fun setValue(index: Int, value: Int) {
+    /**
+     * Set value of index-th block.
+     * Remove possible value from same row, same column and same group blocks of index-th block.
+     */
+    fun setValue(
+        index: Int,
+        value: Int,
+        checkGroup: Boolean = true,
+        checkRow: Boolean = true,
+        checkCol: Boolean = true
+    ) {
         val target = board[index]
         target.setValue(value)
 
-        var blocks = getSameGroup(target)
-        for (block in blocks)
-            block.removePossibleValue(value)
+        if (checkGroup)
+            updateBlocks(target, getSameGroup(target))
 
-        blocks = getSameRow(target)
-        for (block in blocks)
-            block.removePossibleValue(value)
+        if (checkRow)
+            updateBlocks(target, getSameRowOf(target))
 
-        blocks = getSameCol(target)
-        for (block in blocks)
-            block.removePossibleValue(value)
+        if (checkCol)
+            updateBlocks(target, getSameColumnOf(target))
+    }
+
+    private fun updateBlocks(target: Block, blocks: List<Block>) {
+        for (block in blocks) {
+            if (block != target &&
+                block.removePossibleValue(target.getValue()) &&
+                block.getPossibleValuesCount() == 1
+            )
+                setValue(block.index, block.getValue())
+        }
+    }
+
+    private fun checkColumn(_block: Block) {
+        val blocks = getSameColumnOf(_block)
+
+        for (i in 0 until SIZE) {
+            if (blocks[i].getPossibleValuesCount() == 1)
+                continue
+
+            val target = blocks[i]
+            val sameBlocks = blocks.filter { target.hasSamePossibleValues(it) }
+            if (sameBlocks.size < 2 || sameBlocks.size != target.getPossibleValuesCount())
+                continue
+
+            for (diffBlock in blocks.subtract(sameBlocks)) {
+                if (diffBlock.getPossibleValuesCount() == 1)
+                    continue
+                diffBlock.removePossibleValues(target)
+                if (diffBlock.getPossibleValuesCount() == 1) {
+                    setValue(diffBlock.index, diffBlock.getValue())
+                }
+            }
+        }
+    }
+
+    fun checkColumns() {
+        for (i in 0 until SIZE) {
+            checkColumn(board[i])
+        }
+    }
+
+    private fun checkRow(_block: Block) {
+        val blocks = getSameRowOf(_block)
+
+        for (i in 0 until SIZE) {
+            val target = blocks[i]
+            val sameBlocks = blocks.filter { target.hasSamePossibleValues(it) }
+            if (sameBlocks.size < 2 || sameBlocks.size != target.getPossibleValuesCount())
+                continue
+
+            for (diffBlock in blocks.subtract(sameBlocks)) {
+                diffBlock.removePossibleValues(target)
+                if (diffBlock.getPossibleValuesCount() == 1) {
+                    setValue(diffBlock.index, diffBlock.getValue())
+                }
+            }
+        }
+    }
+
+    fun checkRows() {
+        for (i in 0 until SIZE) {
+            checkRow(board[i])
+        }
+    }
+
+    /**
+     * Remove redundant possible values from boards
+     */
+    fun checkGroups(): Boolean {
+        var tryAgain = false
+        val counts = MutableList<Int>(SIZE) { 0 }
+
+        for (groupIndex in 0 until SIZE) {
+            val group = getSameGroup(board[(groupIndex * 3) + (groupIndex / 3) * SIZE * 2])
+
+            for (block in group) {
+                for (value in 1..SIZE) {
+                    if (block.containsPossibleValue(value))
+                        ++counts[value - 1]
+                }
+            }
+
+            for (value in 1..SIZE) {
+                if (counts[value - 1] != 1)
+                    continue
+                for (block in group) {
+                    if (1 < block.getPossibleValuesCount() && block.containsPossibleValue(value)) {
+                        setValue(block.index, value, false)
+                        tryAgain = true
+                    }
+                }
+            }
+            counts.fill(0)
+        }
+
+        // Call checkBoard() again until no block was changed
+        return tryAgain
+    }
+
+    fun getAllPossibleValuesCount(): Int {
+        var count = 0
+        for (block in board) {
+            count += block.getPossibleValuesCount()
+        }
+
+        return count
     }
 }
 
+/**
+ * A block.
+ */
 class Block(val index: Int) {
 
     private val possibleValues = (1..9).toMutableList()
 
     fun setValue(value: Int) {
+        if (getValue() == value)
+            return
+
         possibleValues.clear()
         possibleValues.add(value)
     }
@@ -106,17 +290,25 @@ class Block(val index: Int) {
     fun getValue(): Int {
         return when (possibleValues.size) {
             1 -> possibleValues[0]
-            else -> 0
+            else -> 0 // error!
         }
     }
 
-    fun removePossibleValue(value: Int) {
-        possibleValues.remove(value)
-    }
+    fun removePossibleValue(value: Int) = possibleValues.remove(value)
+
+    fun removePossibleValues(other: Block) = possibleValues.removeAll(other.possibleValues)
+
+    fun containsPossibleValue(value: Int) = possibleValues.contains(value)
+
+    fun getPossibleValuesCount() = possibleValues.count()
+
+    fun hasSamePossibleValues(other: Block) =
+        possibleValues.joinToString("") == other.possibleValues.joinToString("")
 
     override fun toString(): String {
-        return "[$index]$possibleValues"
+        return "${possibleValues.joinToString("", "[", "]").padEnd(11)}"
     }
 
+    override fun hashCode() = index
 }
 
--- app/src/test/java/com/jeongchaeuk/sudoku/ExampleUnitTest.kt
+++ app/src/test/java/com/jeongchaeuk/sudoku/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.jeongchaeuk.sudoku
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +12,77 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun test_Sudoku() {
+        val game = Sudoku()
+        game.makeSample()
+
+        var loopCount = 0
+        while (game.checkGroups()) {
+            ++loopCount
+        }
+        println(game)
+        println("checkGroup try count = $loopCount")
+
+        game.checkColumns()
+        println(game)
+//
+        game.checkRows()
+        println(game)
+
+    }
+
+    @Test
+    fun testList() {
+        val list1 = listOf(Foo(0,1), Foo(1,2), Foo(2,3), Foo(3,1), Foo(4,1))
+        println("list1=$list1")
+
+//        println(list1.count { it.value == list1[0].value })
+
+        val list2 = list1.filter { it.value == list1[0].value }
+        println("list2=$list2")
+
+        val list3 = list1.subtract(list2)
+        println("list3=$list3")
+
+        println(list1.subtract(list3))
+
+        val list4 = listOf(list1[0], list1[1])
+        println("list4=$list4")
+
+        val list5 = list1.subtract(list4)
+        println("list5=$list5")
+
+        val list6 = list1.filter { !list4.contains(it) }
+        println("list6=$list6")
+
+        println(list5.indices)
+
+        list5.elementAt(0).value=100
+        println("$list5, $list1")
+
+
+
+
+
+
+//        println(list.count { it != ' '})
+//
+//        println("[123456789]".padEnd(11, '.'))
+//        println("[123]".padEnd(11, '.'))
+
+
+    }
+
+
+}
+
+data class Foo(private val index: Int, var value: Int) {
+    override fun hashCode() = index
+
+    override fun toString(): String {
+        return " [$index]=$value "
     }
 }
